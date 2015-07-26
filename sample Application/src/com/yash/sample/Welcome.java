9f129b0a500f958b211193127b687dd0e5151876
murali90 <Murali23>
2015-07-26 11:57:45 +0530

    Just Not Started

---
+++ sample Application/src/com/yash/sample/Welcome.java
@@ -0,0 +1,10 @@
+package com.yash.sample;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the git repositery");
+		System.out.println("started work with git repositery ");
+	}
+
+}
